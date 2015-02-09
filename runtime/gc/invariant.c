0e172a773c5550399d8df1375d0c717490c1a03a
Ram Raghunathan <ram.r@cs.cmu.edu>
2015-02-09 15:29:44 -0500

    runtime: Remove trailing whitespace

--- runtime/gc/invariant.c
+++ runtime/gc/invariant.c
@@ -20,8 +20,8 @@ void assertIsObjptrInFromSpace (GC_state s, objptr *opp) {
    * for stacks, the card containing the beginning of the stack is
    * marked, but any remaining cards aren't.
    */
-  if (FALSE and s->mutatorMarksCards 
-      and isPointerInOldGen (s, (pointer)opp) 
+  if (FALSE and s->mutatorMarksCards
+      and isPointerInOldGen (s, (pointer)opp)
       and isObjptrInNursery (s, *opp)
       and not isCardMarked (s, (pointer)opp)) {
     displayGCState (s, stderr);
@@ -63,12 +63,12 @@ bool invariantForGC (GC_state s) {
   }
   /* Generational */
   if (s->mutatorMarksCards) {
-    assert (s->generationalMaps.cardMap == 
+    assert (s->generationalMaps.cardMap ==
             &(s->generationalMaps.cardMapAbsolute
               [pointerToCardMapIndexAbsolute(s->heap->start)]));
     assert (&(s->generationalMaps.cardMapAbsolute
               [pointerToCardMapIndexAbsolute(s->heap->start + s->heap->size - 1)])
-            < (s->generationalMaps.cardMap 
+            < (s->generationalMaps.cardMap
                + (s->generationalMaps.cardMapLength * CARD_MAP_ELEM_SIZE)));
   }
   assert (isAligned (s->heap->size, s->sysvals.pageSize));
@@ -87,7 +87,7 @@ bool invariantForGC (GC_state s) {
     assert (s->limit == s->limitPlusSlop - GC_HEAP_LIMIT_SLOP);
     assert (hasHeapBytesFree (s, 0, 0));
   }
-  assert (s->secondaryHeap->start == NULL 
+  assert (s->secondaryHeap->start == NULL
           or s->heap->size == s->secondaryHeap->size);
   /* Check that all pointers are into from space. */
   foreachGlobalObjptr (s, assertIsObjptrInFromSpace);
@@ -133,13 +133,13 @@ bool invariantForGC (GC_state s) {
 
 bool invariantForMutatorFrontier (GC_state s) {
   GC_thread thread = getThreadCurrent(s);
-  return (thread->bytesNeeded 
+  return (thread->bytesNeeded
           <= (size_t)(s->limitPlusSlop - s->frontier));
 }
 
 bool invariantForMutatorStack (GC_state s) {
   GC_stack stack = getStackCurrent(s);
-  return (getStackTop (s, stack) 
+  return (getStackTop (s, stack)
           <= getStackLimit (s, stack) + getStackTopFrameSize (s, stack));
 }
 
