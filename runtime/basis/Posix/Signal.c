4918284b7255383bfb0c519ddc3cfc0fe5e01771
Matthew Fluet <matthew.fluet@gmail.com>
2015-10-13 15:00:28 -0400

    Drop 'static' from 'struct sigaction sa' in Posix_Signal_handlee

--- runtime/basis/Posix/Signal.c
+++ runtime/basis/Posix/Signal.c
@@ -46,8 +46,7 @@ C_Errno_t(C_Int_t) Posix_Signal_isIgnore (C_Int_t signum, Ref(C_Int_t) isIgn) {
 }
 
 C_Errno_t(C_Int_t) Posix_Signal_handlee (C_Int_t signum) {
-  /* XXX global state */
-  static struct sigaction sa;
+  struct sigaction sa;
 
   sigaddset (GC_getSignalsHandledAddr (), signum);
   memset (&sa, 0, sizeof(sa));
