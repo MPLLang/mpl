MPLLang/mpl

commit ad5189c7a81cd16cf25d8ad13e56871dd07fc565

simplify forwardHHObjptr and remove old promotion support code

runtime/gc/hierarchical-heap-collection.c
@@ -597,52 +597,17 @@ void forwardHHObjptr (GC_state s,
       return;
   }
 
-  /* We look for the top-most collectible replica (tmcr) of p present in the
-   * collection range, that is in [arg->maxLevel, arg->minLevel]. Depending on
-   * what we find, we might have to create a copy or not. There are three cases:
-   *
-   * 1/ The tmcr is in to-space. No copying is needed, the address of the tmcr
-   * is the new location of p.
-   *
-   * 2/ The tmcr is in from-space and has been forwarded. No copy is needed, the
-   * target of the tmcr's forwarding pointer (necessarily outside collection
-   * range) is the new location of p.
-   *
-   * 3/ The tmcr is in from-space and has no forwarding pointer. The tmcr must
-   * be copied in to-space; the resulting address is the new location of p.
-   */
-
-  /* find the top-most collectible replica */
-  p = HM_followForwardPointerUntilNullOrBelowLevel(s,
-                                                   p,
-                                                   args->minLevel);
-  op = pointerToObjptr(p, NULL);
-  HM_getObjptrInfo(s, op, &opInfo);
-
-  if (HM_isObjptrInToSpace(s, op)) {
-    /* just use p/op */
+  if (hasFwdPtr(p)) {
+    op = getFwdPtr(p);
     *opp = op;
-
-    if (DEBUG_DETAILED) {
-      fprintf (stderr, "  already FORWARDED\n");
-    }
-
-    /* objects in to-space should not themselves have forwarding pointers */
-    assert (!hasFwdPtr(p));
-    /* to-space should be copying */
-    assert(COPY_OBJECT_HH_VALUE == opInfo.hh);
-    /* should not have copy-forwarded anything below 'args->minLevel'! */
-    assert (opInfo.level >= args->minLevel);
-  } else if (hasFwdPtr(p)) {
-    /* just use the forwarding pointer of p */
-    *opp = getFwdPtr(p);
-
-    /* should point outside collectible zone */
+    p = objptrToPointer(op, NULL);
 #if ASSERT
-    HM_getObjptrInfo(s, *opp, &opInfo);
-    assert (opInfo.level < args->minLevel);
+    assert(!hasFwdPtr(p));
+    assert(HM_isObjptrInToSpace(s, op));
+    assert(HM_getObjptrLevel(op) >= args->minLevel);
 #endif
   } else {
+    assert(!HM_isObjptrInToSpace(s, op));
     /* forward the object */
     GC_objectTypeTag tag;
     size_t metaDataBytes;

runtime/gc/level.c
@@ -1,128 +1,5 @@
 #include "level.h"
 
-static inline bool inRangeOfHH(struct HM_HierarchicalHeap *hh, Word32 level) {
-  return (hh->stealLevel + 1 <= level && level <= hh->level);
-}
-
-struct HM_HierarchicalHeap *HM_foreachHHUp(GC_state s,
-                                           struct HM_HierarchicalHeap *bottom,
-                                           Word32 minLevel,
-                                           hhFunction function,
-                                           void *arg) {
-  assert (bottom);
-
-  LOG(LM_HIERARCHICAL_HEAP, LL_INFO,
-      "traversing from %p [%u, %u] up to %u",
-      (void *)bottom,
-      bottom->stealLevel + 1,
-      bottom->level,
-      minLevel);
-
-  if (minLevel > bottom->level) {
-    return NULL;
-  }
-
-  for (;
-       bottom && !inRangeOfHH(bottom, minLevel);
-       bottom = bottom->parentHH) {
-    assert (minLevel <= bottom->stealLevel);
-    function(bottom, arg);
-  }
-
-  assert (bottom);
-
-  function(bottom, arg);
-
-  return bottom;
-}
-
-struct hhDownFun_arg {
-  Word32 minLevel;
-  Word32 prevLevel;
-  hhFunction clientFun;
-  void *clientArg;
-};
-
-void hhDownFun(struct HM_HierarchicalHeap *, void *);
-void hhDownFun(struct HM_HierarchicalHeap *hh, void *p) {
-    assert (hh);
-
-    struct hhDownFun_arg *arg = (struct hhDownFun_arg *)p;
-
-    if (inRangeOfHH(hh, arg->minLevel)) {
-      arg->clientFun(hh, arg->clientArg);
-    } else {
-      arg->prevLevel = hh->stealLevel + 1;
-    }
-}
-
-void HM_foreachHHDown(GC_state s,
-                      struct HM_HierarchicalHeap *bottom, Word32 minLevel,
-                      hhFunction fun, void *arg) {
-    assert (bottom);
-
-    /* Since we cannot actually walk HHs from top to bottom, we have to use an
-     * O(N^2) algorithm with two nested loops. In practice N is probably going
-     * to be small enough that this will not matter. */
-
-    struct hhDownFun_arg internalArg = {
-      .minLevel = minLevel,
-      .clientFun = fun,
-      .clientArg = arg,
-    };
-
-    while (bottom != HM_foreachHHUp(s,
-                                    bottom,
-                                    internalArg.minLevel,
-                                    hhDownFun,
-                                    &internalArg)) {
-      assert (internalArg.minLevel < internalArg.prevLevel);
-      internalArg.minLevel = internalArg.prevLevel;
-    }
-}
-
-pointer HM_followForwardPointerUntilNullOrBelowLevel(GC_state s,
-                                                     pointer p,
-                                                     Word32 minLevel) {
-    assert (s);
-    assert (p);
-
-    struct HM_ObjptrInfo info;
-    LOCAL_USED_FOR_ASSERT Word32 prevLevel = minLevel;
-    LOCAL_USED_FOR_ASSERT pointer orig = p;
-
-#if ASSERT
-    HM_getObjptrInfo(s, pointerToObjptr(p, NULL), &info);
-    assert (info.level != CHUNK_INVALID_LEVEL);
-    if (info.level < minLevel) {
-        DIE("pointer %p is already at level %d < %d\n",
-            (void *)p,
-            info.level,
-            minLevel);
-    }
-    prevLevel = info.level;
-#endif  /* ASSERT */
-
-    while (hasFwdPtr(p)) {
-        objptr next = getFwdPtr(p);
-        HM_getObjptrInfo(s, next, &info);
-
-        /* Either we are in to-space, or the level has to decrease strictly. */
-        assert (HM_isObjptrInToSpace(s, next) || prevLevel >= info.level);
-
-        if (info.level < minLevel) {
-            break;
-        }
-        p = objptrToPointer(next, NULL);
-    }
-
-    LOG(LM_HH_PROMOTION, LL_DEBUG,
-        "followed fwd pointers from %p to %p",
-        (void *)orig, (void *)p);
-
-    return p;
-}
-
 bool HM_objptrIsAboveHH(GC_state s,
                         pointer p,
                         struct HM_HierarchicalHeap *hh) {

runtime/gc/level.h
@@ -9,25 +9,6 @@
 
 #if (defined (MLTON_GC_INTERNAL_BASIS))
 
-typedef void (*hhFunction)(struct HM_HierarchicalHeap *, void *);
-
-PRIVATE
-struct HM_HierarchicalHeap *HM_foreachHHUp(GC_state s,
-                                           struct HM_HierarchicalHeap *bottom,
-                                           Word32 minLevel,
-                                           hhFunction function,
-                                           void *arg);
-
-PRIVATE void HM_foreachHHDown(GC_state s,
-                              struct HM_HierarchicalHeap *bottom,
-                              Word32 minLevel,
-                              hhFunction function,
-                              void *arg);
-
-PRIVATE pointer HM_followForwardPointerUntilNullOrBelowLevel(GC_state s,
-                                                             pointer p,
-                                                             Word32 minLevel);
-
 PRIVATE bool HM_objptrIsAboveHH(GC_state s,
                                 pointer p,
                                 struct HM_HierarchicalHeap *hh);

commit 07512dfe2cf5ea1b54a33d106476034bf0cdd0cd

Bugfix: Check already in to-space when forwarding
Not all forwarded objects have fwdptrs, because large objects in
single-object chunks can be forwarded by relinking their chunks
(no copying)

This bug was introduced in ad5189c7a81cd16cf25d8ad13e56871dd07fc565

runtime/gc/hierarchical-heap-collection.c
@@ -594,15 +594,21 @@ void forwardHHObjptr (GC_state s,
       return;
   }
 
+  assert(HM_getObjptrLevel(op) >= args->minLevel);
+
   if (hasFwdPtr(p)) {
     op = getFwdPtr(p);
     *opp = op;
     p = objptrToPointer(op, NULL);
-#if ASSERT
+
     assert(!hasFwdPtr(p));
     assert(HM_isObjptrInToSpace(s, op));
-    assert(HM_getObjptrLevel(op) >= args->minLevel);
-#endif
+
+  } else if (HM_isObjptrInToSpace(s, op)) {
+    /* do nothing; this could be a large object that had its chunk moved
+     * rather than copied */
+    assert(!hasFwdPtr(p));
+
   } else {
     assert(!HM_isObjptrInToSpace(s, op));
     /* forward the object */