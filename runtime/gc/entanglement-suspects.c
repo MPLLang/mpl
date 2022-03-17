b268f2a2c422a7a6609b53bbff5ba9bd609faa04
Jatin Arora <jatina29@gmail.com>
2022-03-17 12:40:20 -0400

    es update

--- runtime/gc/entanglement-suspects.c
+++ runtime/gc/entanglement-suspects.c
@@ -5,32 +5,32 @@ static inline bool suspicious_header(GC_header h) {
 static inline bool mark_suspect(objptr op)
 {
   pointer p = objptrToPointer(op, NULL);
-  // GC_header header = __sync_fetch_and_or(getHeaderp(p), SUSPECT_MASK);
-  // assert (1 == (header & GC_VALID_HEADER_MASK));
-  while (TRUE)
-  {
-    GC_header header = getHeader(p);
-    GC_header newHeader = header | SUSPECT_MASK;
-    if (header == newHeader)
-    {
-      /*
-         just return because the suspect bit is already set
-       */
-      return false;
-    }
-    else
-    {
-      /*
-         otherwise, install the new header with the bit set. this might fail
-         if a concurrent thread changes the header first.
-       */
-      if (__sync_bool_compare_and_swap(getHeaderp(p), header, newHeader))
-        return true;
-    }
-  }
-  DIE("should be impossible to reach here");
+  GC_header header = __sync_fetch_and_or(getHeaderp(p), SUSPECT_MASK);
+  assert(1 == (header & GC_VALID_HEADER_MASK));
+  // while (TRUE)
+  // {
+  //   GC_header header = getHeader(p);
+  //   GC_header newHeader = header | SUSPECT_MASK;
+  //   if (header == newHeader)
+  //   {
+  //     /*
+  //        just return because the suspect bit is already set
+  //      */
+  //     return false;
+  //   }
+  //   else
+  //   {
+  //     /*
+  //        otherwise, install the new header with the bit set. this might fail
+  //        if a concurrent thread changes the header first.
+  //      */
+  //     if (__sync_bool_compare_and_swap(getHeaderp(p), header, newHeader))
+  //       return true;
+  //   }
+  // }
+  // DIE("should be impossible to reach here");
   /*return true if this call marked the header, false if someone else did*/
-  // return !suspicious_header(header);
+  return !suspicious_header(header);
 }
 
 static inline bool is_suspect(objptr op)
@@ -45,30 +45,46 @@ static inline bool is_suspect(objptr op)
 }
 
 void clear_suspect(
-    __attribute__((unused)) GC_state s,
+    GC_state s,
     objptr *opp,
-    __attribute__((unused)) void *rawArgs)
+    void *rawArgs)
 {
-
   objptr op = *opp;
   pointer p = objptrToPointer(op, NULL);
-  while (TRUE)
-  {
-    assert(is_suspect(op)); // no one else should clear it!
-    GC_header header = getHeader(p);
-    GC_header newHeader = header & (~SUSPECT_MASK);
-    // the header may be updated concurrently by a CC
-    if (__sync_bool_compare_and_swap(getHeaderp(p), header, newHeader))
-      return;
+  ES_clearArgs eargs = (ES_clearArgs) rawArgs;
+
+  GC_header header = getHeader(p);
+  uint32_t unpinDepth = (header & UNPIN_DEPTH_MASK) >> UNPIN_DEPTH_SHIFT;
+
+  if (pinType(header) == PIN_ANY && unpinDepth < eargs->heapDepth) {
+    /* Not ready to be cleared */
+    HM_HierarchicalHeap unpinHeap = HM_HH_getHeapAtDepth(s, eargs->thread, unpinDepth);
+    HM_storeInchunkList(HM_HH_getSuspects(unpinHeap), opp, sizeof(objptr));
+    return;
   }
-  DIE("should be impossible to reach here");
-  // assert(isObjptr(op) && is_suspect(p));
-  // __sync_fetch_and_add(getHeaderp(p), ~(SUSPECT_MASK));
+
+  GC_header newHeader = header & ~(SUSPECT_MASK);
+  if (__sync_bool_compare_and_swap(getHeaderp(p), header, newHeader)) {
+    /* clearing successful */
+    return;
+  }
+  else {
+    /*oops something changed in b/w, let's try at the next join*/
+    HM_storeInchunkList(eargs->newList, opp, sizeof(objptr));
+  }
 }
 
 bool ES_contains(__attribute__((unused)) HM_chunkList es, objptr op)
 {
   return is_suspect(op);
+}
+
+bool ES_mark(__attribute__((unused)) GC_state s, objptr op) {
+  return mark_suspect(op);
+}
+
+void ES_unmark(GC_state s, objptr op) {
+  clear_suspect(s, &op, NULL);
 }
 
 void ES_add(__attribute__((unused)) GC_state s, HM_chunkList es, objptr op)
@@ -123,11 +139,20 @@ void ES_move(HM_chunkList list1, HM_chunkList list2) {
   HM_initChunkList(list2);
 }
 
-void ES_clear(GC_state s, HM_chunkList es)
+void ES_clear(GC_state s, HM_HierarchicalHeap hh)
 {
+  HM_chunkList es = HM_HH_getSuspects(hh);
+  uint32_t heapDepth = HM_HH_getDepth(hh);
+  struct HM_chunkList oldList = *(es);
+  HM_initChunkList(HM_HH_getSuspects(hh));
+  struct ES_clearArgs eargs = {
+    .newList = HM_HH_getSuspects(hh),
+    .heapDepth = heapDepth,
+    .thread = getThreadCurrent(s)
+  };
   struct GC_foreachObjptrClosure fObjptrClosure =
-    {.fun = clear_suspect, .env = NULL};
-  int numSuspects = ES_foreachSuspect(s, es, &fObjptrClosure);
+      {.fun = clear_suspect, .env = &(eargs)};
+  int numSuspects = ES_foreachSuspect(s, &oldList, &fObjptrClosure);
   s->cumulativeStatistics->numSuspectsCleared+=numSuspects;
 
   HM_freeChunksInList(s, es);
--- runtime/gc/entanglement-suspects.h
+++ runtime/gc/entanglement-suspects.h
@@ -6,17 +6,26 @@
 #define SUSPECT_MASK ((GC_header)0x40000000)
 #define SUSPECT_SHIFT 30
 
+typedef struct ES_clearArgs {
+  HM_chunkList newList;
+  uint32_t heapDepth;
+  GC_thread thread;
+} * ES_clearArgs;
+
+bool ES_mark(__attribute__((unused)) GC_state s, objptr op);
+void ES_unmark(GC_state s, objptr op);
+
 void ES_add(GC_state s, HM_chunkList es, objptr op);
 
 bool ES_contains(HM_chunkList es, objptr op);
 
-HM_chunkList ES_append (GC_state s, HM_chunkList es1, HM_chunkList es2);
+HM_chunkList ES_append(GC_state s, HM_chunkList es1, HM_chunkList es2);
 
-void ES_clear(GC_state s, HM_chunkList es);
+void ES_clear(GC_state s, HM_HierarchicalHeap hh);
 
 void ES_move(HM_chunkList list1, HM_chunkList list2);
 
-int ES_foreachSuspect(GC_state s, HM_chunkList storage, struct GC_foreachObjptrClosure* fObjptrClosure);
+int ES_foreachSuspect(GC_state s, HM_chunkList storage, struct GC_foreachObjptrClosure * fObjptrClosure);
 
 #endif
 #endif
